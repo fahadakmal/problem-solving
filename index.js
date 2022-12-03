dc663aae7348c6e831ff2bcc3705628878f8a9bb
FAHAD AKMAL <frana7738@gmail.com>
2022-12-03 10:37:27 +0000

    index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const nemo = ['nemo'];
+const large = new Array(100).fill("nemo");
+
+function findNemo(array) {
+  let t1 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log(" found nemo")
+    }
+  }
+  let t2 = performance.now();
+  console.log("performene" + (t2 - t1))
+}
+
+findNemo(nemo)
